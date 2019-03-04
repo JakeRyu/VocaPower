2a92b3a8cf7b31bde935ede816853bf61531320e
Jake Ryu <jake.ryu@hotmail.co.uk>
2019-03-04 22:00:59 +0000

    Add API project

---
+++ VocaPower.Api/Controllers/WordDefinition.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Mvc;
+using VocaPower.Application.Word.Query;
+using VocaPower.Infrastructure.OxfordDictionaryApi;
+
+namespace VocaPower.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WordDefinition : ControllerBase
+    {
+        [Route("{word}")]
+        [HttpGet]
+        public ActionResult LookUp(string word)
+        {
+            var request = new LookUpWordQuery {Word = word};
+            
+            var handler = new LookUpWordQuery.Handler(new OxfordDictionaryApiClient());
+
+            var response = handler.Execute(request);
+            
+            
+            return new JsonResult(response);
+        }
+    }
+}
